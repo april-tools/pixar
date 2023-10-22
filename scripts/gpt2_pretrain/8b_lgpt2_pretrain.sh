bd12bcbe3e0a0163bef83d80aa90cc6ec0e65c5b
TYTTYTTYT <tai.yintao@gmail.com>
2023-10-22 04:14:45 +0100

    change path in scripts to shared folder

--- scripts/gpt2_pretrain/8b_lgpt2_pretrain.sh
+++ scripts/gpt2_pretrain/8b_lgpt2_pretrain.sh
@@ -33,19 +33,25 @@ srun torchrun \
     --rdzv-endpoint=$head_node_ip \
     train.py \
     --model 'LatentGPT2' \
-    --exp_type 'debug' \
-    --backbone_path storage/checkpoints/24D_gpt2_1/lpixel_pretrain/LatentGPT2/20230810-192053/2000/backbone \
-    --compressor_path storage/compressors/CNNAutoencoders/r4h8c1hc16g8b \
+    --exp_type 'dgpt2_pretrain' \
+    --backbone_path /work/sc118/sc118/shared/gpt2 \
     --compressor_name CNNAutoencoder \
-    --dataset_paths storage/enwiki storage/fbookcorpusopen \
+    --compressor_path /work/sc118/sc118/shared/compressors/CNNAutoencoders/r4h8c1hc16g8b \
+    --dataset_paths /work/sc118/sc118/shared/datasets/bookAndwiki \
+    --checkpoint_path /work/sc118/sc118/shared/checkpoints \
+    --max_len 3000 \
+    --dataset_num_shards 256 \
+    --shuffle_dataset true \
     --optim 'AdamW' \
     --lr 6e-4 \
     --beta1 0.9 \
     --beta2 0.95 \
     --decay 0.1 \
-    --total_steps 200 \
-    --save_freq 100 \
-    --best_save_freq 50 \
+    --total_steps 3000000 \
+    --stop_step 100000 \
+    --warm_up_step 2000 \
+    --save_freq 10000 \
+    --eval_freq 5000 \
     --seed 42 \
     --batch_size 384 \
     --sub_size 24 \
@@ -56,11 +62,9 @@ srun torchrun \
     --num_channel 1 \
     --binary true \
     --rgb false \
-    --max_seq_length 2000 \
-    --max_len 3000 \
+    --max_seq_length 1800 \
     --mix_precision fp16 \
     --half_coder false \
     --mp_workers 8 \
     --num_gpu_per_node $GPU_PER_NODE \
     --num_node $NUM_NODES  \
-    
