85c7fa290be46d7d2a355a5566d1e3b2dfe43896
s1891075 <y.tai-6@sms.ed.ac.uk>
2023-12-02 01:36:35 +0000

    Merge flash attention to main branch

--- LatentPixel/modeling/llama.py
+++ LatentPixel/modeling/llama.py
@@ -5,7 +5,7 @@ import torch
 from torch import nn
 from torch.nn import MSELoss, CrossEntropyLoss, BCEWithLogitsLoss
 from transformers import LlamaModel, LlamaConfig
-from transformers.models.llama.modeling_llama import LlamaDecoderLayer, LlamaRMSNorm, _make_causal_mask, _expand_mask, LlamaPreTrainedModel
+from transformers.models.llama.modeling_llama import LlamaAttention, LlamaFlashAttention2, LlamaMLP, LlamaRMSNorm, _make_causal_mask, _expand_mask, LlamaPreTrainedModel
 from transformers.modeling_outputs import BaseModelOutputWithPastAndCrossAttentions, SequenceClassifierOutputWithPast
 from transformers import logging
 from diffusers import AutoencoderKL
@@ -16,9 +16,88 @@ from LatentPixel.text_graph import TGraph
 from .latent_model import LatentModel
 from ..utils import mask2img
 
+#for flash attention
+from transformers.utils import is_flash_attn_available
+if is_flash_attn_available():
+    from flash_attn import flash_attn_func, flash_attn_varlen_func
+    from flash_attn.bert_padding import index_first_axis, pad_input, unpad_input  # noqa
+    
 
 logger = logging.get_logger(__name__)
 
+
+class LlamaDecoderLayer(nn.Module):
+    def __init__(self, config: LlamaConfig):
+        super().__init__()
+        self.hidden_size = config.hidden_size
+        if getattr(config, "flash", True):
+            print("flash attention enabled!")
+        else:
+            print("flash attention disabled!")
+        self.self_attn = (
+            LlamaAttention(config=config)
+            if not getattr(config, "flash", False)
+            else LlamaFlashAttention2(config=config)
+        )
+        self.mlp = LlamaMLP(config)
+        self.input_layernorm = LlamaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+        self.post_attention_layernorm = LlamaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_value: Optional[Tuple[torch.Tensor]] = None,
+        output_attentions: Optional[bool] = False,
+        use_cache: Optional[bool] = False,
+        padding_mask: Optional[torch.LongTensor] = None,
+    ) -> Tuple[torch.FloatTensor, Optional[Tuple[torch.FloatTensor, torch.FloatTensor]]]:
+        """
+        Args:
+            hidden_states (`torch.FloatTensor`): input to the layer of shape `(batch, seq_len, embed_dim)`
+            attention_mask (`torch.FloatTensor`, *optional*): attention mask of size
+                `(batch, 1, tgt_len, src_len)` where padding elements are indicated by very large negative values.
+            output_attentions (`bool`, *optional*):
+                Whether or not to return the attentions tensors of all attention layers. See `attentions` under
+                returned tensors for more detail.
+            use_cache (`bool`, *optional*):
+                If set to `True`, `past_key_values` key value states are returned and can be used to speed up decoding
+                (see `past_key_values`).
+            past_key_value (`Tuple(torch.FloatTensor)`, *optional*): cached past key and value projection states
+        """
+
+        residual = hidden_states
+
+        hidden_states = self.input_layernorm(hidden_states)
+
+        # Self Attention
+        hidden_states, self_attn_weights, present_key_value = self.self_attn(
+            hidden_states=hidden_states,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            past_key_value=past_key_value,
+            output_attentions=output_attentions,
+            use_cache=use_cache,
+            padding_mask=padding_mask,
+        )
+        hidden_states = residual + hidden_states
+
+        # Fully Connected
+        residual = hidden_states
+        hidden_states = self.post_attention_layernorm(hidden_states)
+        hidden_states = self.mlp(hidden_states)
+        hidden_states = residual + hidden_states
+
+        outputs = (hidden_states,)
+
+        if output_attentions:
+            outputs += (self_attn_weights,)
+
+        if use_cache:
+            outputs += (present_key_value,)
+
+        return outputs
 
 class LlamaForPatchCausalInference(LlamaModel):
     
