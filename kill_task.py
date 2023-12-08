af74d257d496c4448693398661cad7fade4babf6
TYTTYTTYT <tai.yintao@gmail.com>
2023-12-08 00:59:35 +0000

    add killer

---
+++ kill_task.py
@@ -0,0 +1,10 @@
+import subprocess
+import os
+process = subprocess.Popen(['ps', '-u'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+out, err = process.communicate()
+
+lines = out.decode().splitlines()
+
+for l in lines:
+    if 'torch' in l:
+        os.system(f'kill -9 {l.split()[1]}')
